47480d78f70ed8fe1497fb05dd24d38e6b3bfd64
olivieresmieutemp <149180068+olivieresmieutemp@users.noreply.github.com>
2023-10-30 10:59:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.write('The user entered ', fruit_choice)
 streamlit.header("The fruit load list contains:")
 #Snowflake related functions
 def get_fruit_load_list():
-    with my_cnx_cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
@@ -58,9 +58,6 @@ if streamlit.button('Get fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 streamlit.stop();
-
-
-my_cur = my_cnx.cursor()
 
 
 
